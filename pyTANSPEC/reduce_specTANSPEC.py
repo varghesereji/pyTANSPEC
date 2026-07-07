a015087094579b01da1f1902ce45fa7a0d5c0031
Varghese Reji <varghese@localhost.localdomain>
2026-07-07 14:44:43 +0530

    In the new version of numpy, the ReFitApertureInXD became in the format ((np.float64(..)..), then it will be saved to the Lamp config as a string because of presence of characters. This caused trouble in extraction that the pipeline take them as string insted of a tuple of floats, so XD fitting was taking place insted of simple extraction. Now, we modified the code to remove the np.float part from it, so only the numbers will be saved to the config file.

--- pyTANSPEC/reduce_specTANSPEC.py
+++ pyTANSPEC/reduce_specTANSPEC.py
@@ -466,9 +466,13 @@ def xdSpectralExtraction_subrout(PC,OutputObjSpecWlCaliList,SpectrumFile,OutputO
     plt.xlabel("Flattened pixels")
     plt.ylabel("Counts")
     plt.show()
-
+    print(Avg_XD_shift)
     # Writing a configuration file for the Lamp
-    ReFitApertureInXD = [tuple(Avg_XD_shift), tuple(PixDomain)]
+    ReFitApertureInXD = (
+        tuple(x.item() if isinstance(x, np.generic) else x for x in Avg_XD_shift),
+        tuple(x.item() if isinstance(x, np.generic) else x for x in PixDomain),
+    )
+    print(ReFitApertureInXD)
     config.set("tracing_settings","ReFitApertureInXD",str(ReFitApertureInXD))
     config.set("tracing_settings","ShowPlot_Trace",str(False))
 
